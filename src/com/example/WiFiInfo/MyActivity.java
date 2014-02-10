02143bee8007304d2f7704c469a58f30e3e311c2
rrajath <rrajath16@yahoo.com>
2014-02-10 02:35:46 -0600

    Add Update button that averages the RSS values and stores each RP info into a hashmap.

--- src/com/example/WiFiInfo/MyActivity.java
+++ src/com/example/WiFiInfo/MyActivity.java
@@ -14,7 +14,10 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class MyActivity extends Activity implements SensorEventListener {
     /**
@@ -23,45 +26,99 @@ public class MyActivity extends Activity implements SensorEventListener {
 
     Sensor acceleration;
     SensorManager sensorManager;
-    TextView textView;
+    TextView tvScans;
+    TextView tvUpdated;
     TextView accelerometer;
+    WifiManager wifiManager;
+    WifiInfo info;
+    String ssid;
+    HashMap<String, Integer> hRss = new HashMap<String, Integer>();
+    HashMap<Integer, HashMap> hRefPoints = new HashMap<Integer, HashMap>();
+    List scanResultsList;
+    int ssCounter;
+    int rpCounter;
+    int sum;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        WifiInfo info = wifiManager.getConnectionInfo();
-//        String ssid = "";
-//                "SSID:" + info.getSSID() + "\n" +
-//                "RSSI:" + info.getRssi() + "\n" +
-//                "Configured Networks:" + wifiManager.getConfiguredNetworks() + "\n" +
-//                "Scan Results:" + wifiManager.getScanResults();
-//                "Calculated Signal Level:" + WifiManager.calculateSignalLevel(info.getRssi(), 10);
-
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        acceleration = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, acceleration, SensorManager.SENSOR_DELAY_NORMAL);
-        accelerometer = (TextView)findViewById(R.id.accelerometer);
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        info = wifiManager.getConnectionInfo();
+//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        acceleration = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//        sensorManager.registerListener(this, acceleration, SensorManager.SENSOR_DELAY_NORMAL);
+//        accelerometer = (TextView)findViewById(R.id.accelerometer);
     }
 
     public void scan(View view) {
+        // Every time the scan button is pressed, the RSS value for each AP will be captured
+        // and added to the corresponding array of size 4
+
+        // Start the wifi scan
         WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         wifiManager.startScan();
 
-        String ssid = new String();
+        // Capture SSID
+        ssid = "";
+        String scans = "";
 
-        List scanResultsList = wifiManager.getScanResults();
+        // List of access points scanned
+        scanResultsList = wifiManager.getScanResults();
+
+        // Iterate through the scan results and add new RSS values to hashmap
         for (int i=0; i<scanResultsList.size(); i++) {
             ScanResult scanResult = (ScanResult)scanResultsList.get(i);
-            ssid += scanResult.SSID + " | " + scanResult.BSSID + ":" +
-                    WifiManager.calculateSignalLevel(scanResult.level, 100) + "\n";
+            ssid = scanResult.SSID;
+            if (hRss.get(ssid) != null) {
+                sum = (int)hRss.get(ssid);
+                sum += WifiManager.calculateSignalLevel(scanResult.level, 100);
+            }
+            else {
+                sum = WifiManager.calculateSignalLevel(scanResult.level, 100);
+            }
+            hRss.put(ssid, sum);
+            scans += ssid + " : " + sum + "\n";
         }
-        textView = (TextView)findViewById(R.id.textView);
-        textView.setMovementMethod(new ScrollingMovementMethod());
-        textView.setText(ssid);
+        ssCounter++;
 
+        tvScans = (TextView)findViewById(R.id.tvScan);
+        tvScans.setMovementMethod(new ScrollingMovementMethod());
+        tvScans.setText(scans);
+
+    }
+
+    // Avg of values will be taken and HashMap will be replaced with the final value.
+    public void update(View view) {
+        int ssSum = 0;
+        String output = "Final Output:\n";
+
+        // Iterate through the RSS HashMap to take the average value of RSSs from surrounding
+        // access points
+        for (Map.Entry<String, Integer> entry : hRss.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            value = value/ssCounter;
+            hRss.put(key, value);
+
+            output += key + " : " + value + "\n";
+        }
+
+        // Increment the ref point counter so that the vector of RSS values are mapped to the
+        // corresponding access points
+        rpCounter++;
+
+        // Reset the signal strength counter to get new set of RSS values for the next ref point
+        ssCounter = 0;
+
+        // Add ref points to a hashmap as (ref_pt_no, hRss) and clear the old hRss hashmap
+        hRefPoints.put(rpCounter, hRss);
+        hRss.clear();
+
+        tvUpdated = (TextView)findViewById(R.id.tvUpdated);
+        tvUpdated.setMovementMethod(new ScrollingMovementMethod());
+        tvUpdated.setText(output);
     }
 
     @Override
